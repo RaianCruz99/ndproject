ca105e4a38b3c609a042deeefd1484150a71a68c
RaianCruz99 <raian.cruz@ucal.edu.br>
2023-11-24 10:00:13 -0300

    Adicionando novos recursos: descrever

--- scripit.js
+++ scripit.js
@@ -1,3 +1,28 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("inscricao");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
   document.addEventListener('DOMContentLoaded', function () {
     // Obtém a referência para o elemento do calendário
     var calendar = document.getElementById('calendar');
