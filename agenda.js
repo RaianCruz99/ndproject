683e8aaf7b1116ec6a3ae73f8690d37a5f5e8492
RaianCruz99 <raian.cruz@ucal.edu.br>
2023-12-04 14:55:20 -0300

    finalizando footer, agenda e botões nav bar.

--- agenda.js
+++ agenda.js
@@ -5,8 +5,9 @@ document.addEventListener("DOMContentLoaded", function () {
     initialView: "listMonth",
     buttonText: {
       today: 'Mês Atual'
-   },
+    },
     events: [
+      /*NOVEMBRO*/
       {
         title: "Culto Presencial",
         start: "2023-11-25T22:00:00",
@@ -30,25 +31,40 @@ document.addEventListener("DOMContentLoaded", function () {
       {
         title: "Podcast",
         start: "2023-11-26T20:00:00",
+        extendedProps: {
+          status: "green",
+        },
+      },
+      
+      /*DEZEMBRO*/
+      {
+        title: "Culto Presencial",
+        start: "2023-12-02T22:00:00",
+        extendedProps: {
+          status: "done",
+        },
+      },
+      {
+        title: "Culto Online no Youtube",
+        start: "2023-12-02T20:00:00",
+        backgroundColor: "green",
+        borderColor: "done",
+      },
+      {
+        title: "Culto Presencial",
+        start: "2023-12-04T20:00:00",
+        extendedProps: {
+          status: "green",
+        },
+      },
+      {
+        title: "Apresentação do nosos Site",
+        start: "2023-12-07T20:00:00",
         backgroundColor: "green",
         borderColor: "green",
       },
+
     ],
-
-
-    eventClick: function (info) {
-      // Exibir informações do evento no modal
-      $("#eventoModalTitle").text(info.event.title);
-      $("#eventoModalBody").html(
-        "<p>Data: " +
-          info.event.start.toLocaleString() +
-          "</p>" +
-          "<p>Outras informações do evento...</p>"
-      );
-
-      // Abrir o modal
-      $("#eventoModal").modal("show");
-    },
   });
 
   calendar.render();
